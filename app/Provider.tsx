b80cf882d7a94ed96dc9e6544eac9c83fb277bbe
kosuke9809 <uht0912k@outlook.com>
2024-08-04 23:15:00 +0900

    complete

---
+++ app/Provider.tsx
@@ -0,0 +1,33 @@
+'use client'
+import Loader from '@/components/Loader'
+import { getClerkUsers, getDocumentUsers } from '@/lib/actions/user.actions'
+import { LiveblocksProvider, ClientSideSuspense } from '@liveblocks/react'
+import { useUser } from '@clerk/nextjs'
+import { ReactNode } from 'react'
+
+const Provider = ({ children} : { children: ReactNode}) => {
+  const { user: clerkUser } = useUser()
+  return (
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      resolveUsers={async({ userIds }) => {
+        const users = await getClerkUsers({ userIds })
+        return users
+      }}
+      resolveMentionSuggestions={ async({ text, roomId }) => {
+        const roomUsers = await getDocumentUsers({
+          roomId,
+          currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+          text
+        })
+        return roomUsers
+      }}
+    >
+        <ClientSideSuspense fallback={<Loader/>}>
+            {children}
+        </ClientSideSuspense>
+    </LiveblocksProvider>
+  )
+}
+
+export default Provider
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://8e84666117d127f661dd4d3d23e9fdab@o4507717744787456.ingest.us.sentry.io/4507717748260864",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
